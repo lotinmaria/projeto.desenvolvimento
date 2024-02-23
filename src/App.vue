32b3f883bb8a5b84b8a644c0c749953ede070f98
lotinmaria <mariaholotin2007@gmail.com>
2024-02-23 11:49:23 -0300

    feito

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,27 @@ import { ref } from 'vue'
 const contador = ref(0)
 
 function decrementar(){
-  contador.value--
+  if (contador.value >0){contador.value--}
 }
+
+function reset(){
+  contador.value = 0
+  
+}
+
 </script>
 
 <template> 
 <button @click="contador++">+</button>
 <button @click="decrementar">-</button>
+<button @click="reset">reset</button>
 <p> valor do contador: {{ contador }}</p>
 </template>
 
-<style scoped></style>
+<style scoped>
+button {
+  font-weight: bold;
+  color: rgb(3, 90, 18);
+  background-color: rgb(22, 201, 156);
+  border: 2;
+}</style>
